adccbc198672b6cf075c3a81f31b44a0a43c0fc0
Scrappers <scrappers.tm@gmail.com>
2022-02-03 14:32:16 +0200

    FixedBlinking example to not override other bits in the register

--- HelloBlink/main/libs/Digital.c
+++ HelloBlink/main/libs/Digital.c
@@ -10,27 +10,28 @@
  */
 #include<Digital.h>
 
-/**
- * Resets the specified port, the port is in unsigned 8-bit integer.
- * An avr register represents a PORT with 8 pins.
- * 
- * @param PORT the port to reset.
- */
+volatile uint8_t cache;
+
 void resetPort(volatile uint8_t& PORT) {
     PORT = 0x00;
 }
 
-/**
- * Digital writes [HIGH] or [LOW] to a pin on a register.
- * 
- * @param PORT the register port ID.
- * @param PIN the bin ID in binaries.
- * @param POWER trigger the power, either HIGH or LOW.
- */
-void digitalWrite(volatile uint8_t& PORT, unsigned char PIN, unsigned char POWER) {
-  // 1) flip all the port bits
-  // then pass the bits to an and gate with the powered pin
-  // if the powered pin has all its bits turned to 1 then the result that all bits would be turncated to 1
-  // so, only the true bits (the bits that equals to 1) on both sides of AND would be activated.
-  PORT = ~PORT & (PIN & POWER);
+void resetPin(volatile uint8_t& PORT, const uint8_t& PIN) {
+    PORT = PORT & (~PIN);
+}
+
+void powerPin(volatile uint8_t& PORT, const uint8_t& PIN) {
+    PORT = PORT | PIN;
+}
+
+void digitalWrite(volatile uint8_t& PORT, unsigned char PIN, const int& POWER) {
+  // assign the port to a powered pin
+  // if power = HIGH, then the bit is activated.
+  // if power = LOW, then the bit is de-activated.
+  if (POWER == LOW) {
+      resetPin(PORT, PIN);
+  } else {
+      powerPin(PORT, PIN);
+  }
+//   PORT = PIN & POWER;
 }
--- HelloBlink/readMe.md
+++ HelloBlink/readMe.md
@@ -5,20 +5,19 @@ Shows an example of toggling PORTB1 (PB1) bit of PORTB register of the atmega328
 ### Example 2 : 
 - Blinks the LED on PB0 or D9 bit of PORTB register.
 - Idea : 
-    - Since, `PORTB1 = 0b00000010`, then to switch it on, simply we `flip all the bits on register B` and then `AND` the result with the `PORTB1` :
-    ```c
-    PORTB1 = 0b000000010;
-    PORTB = ~PORTB & PORTB1;
-    ```
-    - Now, we could do one more step to toggle the PB1 bit `POWER` selectively, which is `ANDING` the `PORTB1` with either `0xFF` which is `HIGH` or `0x00` which is `LOW` :
-    ```c
-     #define HIGH 0xFF
-     #define LOW 0x00
-     
-     PORTB1 = 0b000000010;
-     POWER = HIGH;
-     PORTB = ~PORTB & (PORTB1 & POWER);
-     ```
+    - 2 methods to consider : 
+	```c
+	void resetPin(volatile uint8_t& PORT, const uint8_t& PIN) {
+	    PORT = PORT & (~PIN);
+	}
+
+	void powerPin(volatile uint8_t& PORT, const uint8_t& PIN) {
+	    PORT = PORT | PIN;
+	}
+	```
+	- `resetPin()` : resets the selected bit in a register to zero by flipping all the other bits then
+	   `ANDING` the result with the current `PORT` values, this makes sure that we don't override or 		    change the other bits when altering a specific bit.
+	- `powerPin()` : powers the selected bit in a register by `ORING` the bit with the `PORT`, so if the 	        bit is ZERO, its now on and if other bits are ONE they would stay ONE.
 - The code based on this circuit : 
 
 
