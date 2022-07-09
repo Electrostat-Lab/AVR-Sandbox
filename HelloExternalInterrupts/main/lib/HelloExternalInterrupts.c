9034add21c1b5415555e54e0f519cd6843792b57
Scrappers <scrappers.tm@gmail.com>
2022-07-09 15:47:44 +0200

    HelloExternalInterrupts: Better handlers

--- HelloExternalInterrupts/main/lib/HelloExternalInterrupts.c
+++ HelloExternalInterrupts/main/lib/HelloExternalInterrupts.c
@@ -17,9 +17,16 @@ void Serial::UART::onDataTransmitCompleted(const uint8_t& data) {
  */
 void GPIO::ExternalInterruptHandler::onPinChanged() {
       /* print on the RISING edge (when PD4 is still LOW) -- FROM LOW TO HIGH Logic*/
-      if (!isPCINT21Active()) {
+      if (!isPCINT20Active()) {
           Serial::UART::getInstance()->sprintln((char*) "ON -- RISING EDGE");
+          /* set the pin to HIGH LEVEL as of after the RISING EDGE */
+          PORTD |= (1 << PD4);
+      } else if (isPCINT20Active()) {
+          Serial::UART::getInstance()->sprintln((char*) "OFF -- FALLING EDGE");
+          /* set the pin to LOW LEVEL as of after the FALLING EDGE */
+          PORTD &= ~(1 << PD4);
       }
+      reti(); /* exit to the main function */
 }
 
 int main(void) {
@@ -30,9 +37,11 @@ int main(void) {
     GPIO::ExternalInterruptHandler::getInstance()->activatePCINT2(1 << PCINT20);
     
     /* block as long as the pin is on the ZERO LEVEL */
-    while (!isPCINT21Active()) { 
-        Serial::UART::getInstance()->sprintln((char*) "OFF -- LOW LEVEL");
-        _delay_ms(500);
+    while (true) { 
+        if (!isPCINT20Active()) {
+            Serial::UART::getInstance()->sprintln((char*) "OFF -- LOW LEVEL");
+        }
+        _delay_ms(1000);
     } 
 
     while (1);
