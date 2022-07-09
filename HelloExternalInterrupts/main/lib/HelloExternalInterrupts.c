df4e661d945dd43fbee73fe5a4007a4db02f5298
Scrappers <scrappers.tm@gmail.com>
2022-07-09 17:50:30 +0200

    Code and Files: Removed test changes

--- HelloExternalInterrupts/main/lib/HelloExternalInterrupts.c
+++ HelloExternalInterrupts/main/lib/HelloExternalInterrupts.c
@@ -5,7 +5,6 @@
 #include<DigitalServiceHandler.h>
 
 void Serial::UART::onDataReceiveCompleted(const uint8_t& data) {
-          Serial::UART::getInstance()->println(data, 10);
 
 }
 
@@ -32,18 +31,18 @@ void GPIO::ExternalInterruptHandler::onPinChanged() {
 
 int main(void) {
     Serial::UART::getInstance()->startProtocol(BAUD_RATE_57600);
-    // /* Write PD4 to LOW */
-    // PORTD &= ~(1 << PD4);
-    // /* activate the external interrupt service routine handler on the PCINT20/PD4 */
-    // GPIO::ExternalInterruptHandler::getInstance()->activatePCINT2(1 << PCINT20);
+    /* Write PD4 to LOW */
+    PORTD &= ~(1 << PD4);
+    /* activate the external interrupt service routine handler on the PCINT20/PD4 */
+    GPIO::ExternalInterruptHandler::getInstance()->activatePCINT2(1 << PCINT20);
     
-    // /* block as long as the pin is on the ZERO LEVEL */
-    // while (true) { 
-    //     if (!isPCINT20Active()) {
-    //         Serial::UART::getInstance()->sprintln((char*) "OFF -- LOW LEVEL");
-    //     }
-    //     _delay_ms(1000);
-    // } 
+    /* block as long as the pin is on the ZERO LEVEL */
+    while (true) { 
+        if (!isPCINT20Active()) {
+            Serial::UART::getInstance()->sprintln((char*) "OFF -- LOW LEVEL");
+        }
+        _delay_ms(1000);
+    } 
 
     while (1);
     return 0;
