d06c17473c3345897ed1b39f3b9b80a3b10ddcf2
Scrappers <scrappers.tm@gmail.com>
2022-07-30 12:11:11 +0200

    AvrDude: Initial Commit

---
+++ AvrDudeTutorial/main/lib/HelloBlink.c
@@ -0,0 +1,27 @@
+/**
+ * Blinks an LED at D9 or PB1.
+ * 
+ * @file HelloBlink.c
+ * @author pavl_g.
+ * @version 0.1
+ * @date 2022-02-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include<avr/io.h>
+#include<util/delay.h>
+#include<Digital.h>
+
+int main(void) {
+  resetPin(PORTB, PIN_5);
+ for(;;) {
+    digitalWrite(PORTB, PIN_5, HIGH);
+    _delay_ms(1000);
+    digitalWrite(PORTB, PIN_5, LOW);
+    _delay_ms(1000);
+ }
+ return 0;
+}
+
+
