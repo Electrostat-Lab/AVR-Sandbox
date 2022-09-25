081b40aab1ffaabc134e25703393fb435b7abaa4
pavl_g <scrappers.tm@gmail.com>
2022-09-25 20:23:27 +0200

    HelloMPLab: Added a MPLab avr IDE example using GNU Makefile

---
+++ HelloMPLab.X/HelloWorld.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   HelloWorld.c
+ * Author: pavl-machine
+ *
+ * Created on September 24, 2022, 8:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <util/delay.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ HelloMPLab.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HelloMPLab.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HelloMPLab.X.production.hex
